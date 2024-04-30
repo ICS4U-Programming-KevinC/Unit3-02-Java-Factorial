8574ef827c93291598dd21962ef7b88faec9b3ab
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-04-30 14:29:07 +0000

    Fixed linter pt1

--- com/example/Factorial.java
+++ com/example/Factorial.java
@@ -15,52 +15,55 @@ import java.util.Scanner;
 // Factorial class
 public final class Factorial {
 
-    /** Private constructor to prevent instantiation. */
-    private Factorial() {
-        throw new UnsupportedOperationException("Cannot instantiate");
+  /** Private constructor to prevent instantiation. */
+  private Factorial() {
+    throw new UnsupportedOperationException("Cannot instantiate");
+  }
+
+  /**
+   * This is the main method.
+   *
+   * @param args Unused
+   */
+  public static void main(final String[] args) {
+    try {
+      // Setup scanner on file.
+      File file = new File("input.txt");
+      Scanner sc = new Scanner(file);
+      // Setup writer for output file.
+      FileWriter writer = new FileWriter("output.txt");
+      BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+      while (sc.hasNextLine()) {
+        // Read the line from file.
+        String line = sc.nextLine();
+
+        // Call method and write to file.
+        int result = recFactorial(Integer.parseInt(line));
+        bufferedWriter.write(Integer.toString(result));
+        bufferedWriter.newLine();
+      }
+
+      // Close all writers and scanner.
+      bufferedWriter.close();
+      writer.close();
+      sc.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * This is the main method.
-     *
-     * @param args Unused
-     */
-    public static void main(final String[] args) {
-        try {
-            // Setup scanner on file.
-            File file = new File("input.txt");
-            Scanner sc = new Scanner(file);
-            // Setup writer for output file.
-            FileWriter writer = new FileWriter("output.txt");
-            BufferedWriter bufferedWriter = new BufferedWriter(writer);
-
-            while (sc.hasNextLine()) {
-                // Read the line from file.
-                String line = sc.nextLine();
-
-                // Call method and write to file.
-                int result = recFactorial(Integer.parseInt(line));
-                bufferedWriter.write(Integer.toString(result));
-                bufferedWriter.newLine();
-            }
-
-            // Close all writers and scanner.
-            bufferedWriter.close();
-            writer.close();
-            sc.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  /**
+   * Calculates the factorial recursively.
+   *
+   * @param num
+   * @return Factorial of input.
+   */
+  public static Integer recFactorial(final Integer num) {
+    if (num <= 1) {
+      return 1;
+    } else {
+      return num * recFactorial(num - 1);
     }
-
-    public static Integer recFactorial(Integer num)
-    {
-        if (num <= 1)
-        {
-            return 1;
-        } else
-        {
-            return num * recFactorial(num - 1);
-        }
-    }
+  }
 }
